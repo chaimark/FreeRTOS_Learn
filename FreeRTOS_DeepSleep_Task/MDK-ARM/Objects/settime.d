6c5729edbdc884b6f996e4e789b3b32cbd093286
chai21sks <3046476712@qq.com>
2025-09-13 16:35:55 +0800

    del build obj

--- FreeRTOS_DeepSleep_Task/MDK-ARM/Objects/settime.d
+++
@@ -1,7 +0,0 @@
-.\objects\\settime.o: ..\Interflow\SetTime.c
-.\objects\\settime.o: ..\Interflow\SetTime.h
-.\objects\\settime.o: ..\Interflow\StrLib.h
-.\objects\\settime.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\\settime.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\\settime.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\\settime.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- FreeRTOS_DeepSleep_Task/MDK-ARM/Objects/startup_fm33lc0xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\\startup_fm33lc0xx.o: ../Drivers/CMSIS/Device/FM/FM33xx/Source/Templates/arm/startup_fm33lc0xx.s
